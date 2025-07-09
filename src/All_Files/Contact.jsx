5f5ac50a4403b9cfb4852739fa2799a3680fd1ae
varun-004 <gavarunvarun@gmail.com>
2025-07-09 13:14:56 +0530

    changed the contact page

--- src/All_Files/Contact.jsx
+++ src/All_Files/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 
 export default function Contact() {
   return (
@@ -33,7 +33,7 @@ export default function Contact() {
 
         <div className='w-[100%] h-[25%] sm:w-[100%] sm:h-[35%] bg-blue-300 flex flex-col pl-10 pt-2 '>
           <label className='text-lg sm:text-xl'>Message</label>
-          <input type='text' placeholder='please enter message' className='w-[90%] h-[65%] sm:w-[90%] sm:h-[70%] border-x-2 border-y-2 border-black'></input>
+          <input type='text' placeholder='Please enter message' className='w-[90%] h-[65%] sm:w-[90%] sm:h-[70%] border-x-2 border-y-2 border-black'></input>
         </div>
 
         <div className='w-[100%] h-[10%] bg-red-200 pl-10 flex items-center'>
@@ -43,11 +43,11 @@ export default function Contact() {
 
       </div>
       <div className='w-[90%] h-[5%] sm:w-[50%] sm:h-[5%] flex justify-around sm:justify-evenly sm:pt-8'>
-        <a href='https://api.whatsapp.com/send/?phone=9036287917&text&type=phone_number&app_absent=0'><i id="social_media" class="fi fi-brands-whatsapp"></i></a>
-        <a href="https://www.instagram.com/varun_g_a_04/" ><i id="social_media" class="fi fi-brands-instagram"></i></a>
-        <a href="https://app.slack.com/client/T07JQA2F570/C07JU2WV9MK" ><i id="social_media" class="fi fi-brands-slack"></i></a>           
-        <a href="https://www.linkedin.com/in/varun-g-a-112a4b301/" ><i id="social_media" class="fi fi-brands-linkedin"></i></a>       
-        <a href="https://x.com/Varun_g_a_" ><i id="social_media" class="fi fi-brands-twitter-alt-circle"></i></a>
+        <a href='https://api.whatsapp.com/send/?phone=9036287917&text&type=phone_number&app_absent=0'><i id="social_media" className="fi fi-brands-whatsapp"></i></a>
+        <a href="https://www.instagram.com/varun_g_a_04/" ><i id="social_media" className="fi fi-brands-instagram"></i></a>
+        <a href="https://app.slack.com/client/T07JQA2F570/C07JU2WV9MK" ><i id="social_media" className="fi fi-brands-slack"></i></a>           
+        <a href="https://www.linkedin.com/in/varun-g-a-112a4b301/" ><i id="social_media" className="fi fi-brands-linkedin"></i></a>       
+        <a href="https://x.com/Varun_g_a_" ><i id="social_media" className="fi fi-brands-twitter-alt-circle"></i></a>
       </div>
       
 
